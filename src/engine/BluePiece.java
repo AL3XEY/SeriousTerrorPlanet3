5b23cbc9493df4f25578a0c2c7369fea8578ac52
Alexandre Faucher <allemagne7@hotmail.fr>
2016-09-28 12:15:55 +0200

    Add pieces graphical differentiation

--- src/engine/BluePiece.java
+++ src/engine/BluePiece.java
@@ -1,5 +1,9 @@
 package engine;
 
 public class BluePiece extends Piece {
+	
+	public String toString(){
+		return "X";
+	}
 
 }
--- src/engine/RedPiece.java
+++ src/engine/RedPiece.java
@@ -1,5 +1,9 @@
 package engine;
 
 public class RedPiece extends Piece {
+	
+	public String toString(){
+		return "O";
+	}
 
 }
