9864423f9302b5502a5631225fd6730b12f6f962
Alexandre Faucher <allemagne7@hotmail.fr>
2016-09-28 12:44:51 +0200

    Add ability to move pieces

--- src/engine/Game.java
+++ src/engine/Game.java
@@ -10,4 +10,23 @@ public class Game {
 		map = new Map();
 	}
 	
+	public void battle(){
+		//for
+	}
+	
+	public void movePiece(Player player, int i, int j){
+		Piece piece;
+		if(player.equals(redPlayer)){
+			piece = new RedPiece();
+		}else{
+			piece = new BluePiece();
+		}
+		
+		try{
+			map.movePiece(piece, i, j);
+		}catch(NoPieceInCellException e){
+			System.err.println(e);
+		}
+	}
+	
 }
