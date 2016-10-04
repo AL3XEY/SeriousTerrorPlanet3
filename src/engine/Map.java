02d5dba28b40435f8fa6c6d73b870cdb24c861ab
Alexandre Faucher <allemagne7@hotmail.fr>
2016-10-04 17:17:14 +0200

    Add toString + battle and move (WIP)

--- src/engine/Map.java
+++ src/engine/Map.java
@@ -5,8 +5,6 @@ import java.util.List;
 public class Map {
 	
 	private int[][] cells;
-
-	//private Cell[] cells;
 	
 	public Map(){
 		cells = new int[2][12];
@@ -23,25 +21,84 @@ public class Map {
 	}
 
 	public void battle() {
-		// TODO Auto-generated method stub
+		int firstPlayerPieces;
+		int secondPlayerPieces;
+		int foo;
+		for(int i=1; i<11;i++){
+			firstPlayerPieces = cells[0][i];
+			secondPlayerPieces = cells[1][i];
+			if(firstPlayerPieces > 0 && secondPlayerPieces > 0){
+				foo = firstPlayerPieces - secondPlayerPieces;
+				cells[0][i]-=Math.abs(foo);
+				cells[1][i]-=Math.abs(foo);
+				cells[0][0]+=Math.abs(foo);
+				cells[1][0]+=Math.abs(foo);
+			}
+		}
 		
 	}
 
-	public void movePiece(int player, int caseDepart, int caseArrivee) {
-		// TODO Auto-generated method stub
+	public void movePiece(int player, int caseDepart, int caseArrivee){
+		// TODO Exceptions, verifications and co.
+		cells[player][caseDepart]--;
+		cells[player][caseArrivee]++;
+	}
+	
+	public int testMove(int player, int caseDepart, int caseArrivee, int pointsDeplacement){
+		//TODO test if correct move number
 		
+		//TODO test if valid move (for the special last cell)
+		
+		return -1;
 	}
 	
-	/*public Cell[] getCells(){
-		return cells;
+	// method test finalMove
+	
+	public String toString(){
+		String output = "";
+		
+		//top
+		output += " s |";
+		for(int i=1; i<11; i++){
+			output += " " + i + " |";
+		}
+		output += " e |\n";
+		
+		//x
+		
+		for(int i=1; i<12; i++){
+			int piecesCount = cells[0][i];
+			for(int j=0;j<piecesCount;j++){
+				output += "X";
+			}
+			for(int j=0;j<3-piecesCount;j++){
+				output += " ";
+			}
+			output += "|";
+		}
+		output += "\n";
+		
+		//separation
+		for(int i=0; i<12; i++){
+			output += "---|";
+		}
+		output += "\n";
+	
+		//o
+		
+		for(int i=1; i<12; i++){
+			int piecesCount = cells[1][i];
+			for(int j=0;j<piecesCount;j++){
+				output += "O";
+			}
+			for(int j=0;j<3-piecesCount;j++){
+				output += " ";
+			}
+			output += "|";
+		}
+		output += "\n";
+		
+		
+		return output;
 	}
-	
-	public Cell getCell(int i){
-		return cells[i];
-	}
-	
-	public List<Piece> getPieces(int i){
-		return cells[i].getPieces();
-	}*/
-	
 }
